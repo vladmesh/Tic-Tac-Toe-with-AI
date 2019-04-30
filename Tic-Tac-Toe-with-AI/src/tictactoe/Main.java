5a9c828ea286f4180dae156705b9ab4cf2eb0d22
vladislav <vladiclav.me@gmail.com>
2019-04-30 19:31:32 +0600

    Add main menu

--- Tic-Tac-Toe-with-AI/src/tictactoe/Main.java
+++ Tic-Tac-Toe-with-AI/src/tictactoe/Main.java
@@ -11,37 +11,46 @@ public class Main {
         final char[] tokens = {'X', 'O'};
         final Player[] players = new Player[2];
         while (true) {
-            System.out.println("Input command");
+            System.out.println("Input command\n" +
+                    "1. New game\n" +
+                    "2. Exit");
             command = scanner.next();
-            if ("exit".equals(command)) {
+            if ("2".equals(command)) {
                 break;
             }
-
             try {
-                if (!"start".equals(command)) throw new IllegalArgumentException();
-                for (int i = 0; i < 2; i++) {
-                    String s = scanner.next();
-                    switch (s) {
-                        case "user":
-                            players[i] = new User(tokens[i], scanner);
-                            break;
-                        case "easy":
-                            players[i] = new EasyAI(tokens[i]);
-                            break;
-                        case "medium":
-                            players[i] = new MediumAI(tokens[i]);
-                            break;
-                        case "hard":
-                            players[i] = new HardAI(tokens[i]);
-                            break;
-                        default:
-                            throw new IllegalArgumentException();
+                if ("1".equals(command)) {
+                    for (int i = 0; i < 2; i++) {
+                        System.out.println("Choose a " + tokens[i] + " Player:\n" +
+                                "1. Human\n" +
+                                "2. Easy AI\n" +
+                                "3. Medium AI\n" +
+                                "4. Hard AI\n");
+                        command = scanner.next();
+
+                        switch (command) {
+                            case "1":
+                                players[i] = new User(tokens[i], scanner);
+                                break;
+                            case "2":
+                                players[i] = new EasyAI(tokens[i]);
+                                break;
+                            case "3":
+                                players[i] = new MediumAI(tokens[i]);
+                                break;
+                            case "4":
+                                players[i] = new HardAI(tokens[i]);
+                                break;
+                            default:
+                                throw new IllegalArgumentException();
+                        }
                     }
+                    Game game = new Game(new Field(3), players[0], players[1]);
+                    game.start();
                 }
-                Game game = new Game(new Field(3), players[0], players[1]);
-                game.start();
+                else throw new IllegalArgumentException();
             }
-            catch (IllegalArgumentException e){
+            catch(IllegalArgumentException e){
                 System.out.println("Bad parameters!");
             }
         }
